b73fa4e6d33df086df52c102cbca04f8ea762f63
Ekaterina <b.ekaterina098@gmail.com>
2022-06-15 14:48:29 +0300

    countSmileys

--- smileys.test.js
+++ smileys.test.js
@@ -32,7 +32,10 @@ test("Valid smileys should return 0", () => {
     expect(countSmileys([':3', ':8', '=D', '<3'])).toBe(0);
   });
 
-  test("Valid smileys should return 4", () => {
-    expect(countSmileys([':~D',':~)',';~D',':~)'])).toBe(4);
+  test("Valid smileys should return 6", () => {
+    expect(countSmileys([':~D',':~)',';~D',':~)',':-)',';-D'])).toBe(6);
   });
 
+  test("Valid smileys should return 0", () => {
+    expect(countSmileys([8])).toBe(0);
+  });
